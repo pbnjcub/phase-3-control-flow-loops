a48a3e917a1fafb8c8ccd7e4dcfaac303d342255
Paul Jung <pbnjcub@gmail.com>
2023-02-09 18:06:27 -0500

    commit tree 0c5cab4082a4b4a405ecb6488022e61869913ab5

--- looping.rb
+++ looping.rb
@@ -1,5 +1,4 @@
 def happy_new_year
-  # your code here
   i = 10
   while i > 0
     puts "#{i}"
@@ -8,21 +7,7 @@ def happy_new_year
   puts "Happy New Year!"
 end
 
-# No need to modify this code! Use this to implement the fizzbuzz_printer method.
-def fizzbuzz(num)
-  if num % 3 == 0 && num % 5 == 0
-    "FizzBuzz"
-  elsif num % 3 == 0
-    "Fizz"
-  elsif num % 5 == 0
-    "Buzz"
-  else
-    num
-  end
-end
-
 def fizzbuzz_printer
-  # your code here
   (1..100).each do |num|
     if num % 5 == 0 && num % 3 == 0
       puts "FizzBuzz"
@@ -37,12 +22,89 @@ def fizzbuzz_printer
 end
 
 def reverse_string(str)
-  # your code here
-  s
   str.chars.reduce { |x, y| y + x }
-  # i = str.length - 1
-  # while i >= 0
-  #   print char[i]
-  #   i = i - 1
-  # end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def happy_new_year
+#   # your code here
+#   i = 10
+#   while i > 0
+#     puts "#{i}"
+#     i -= 1
+#   end
+#   puts "Happy New Year!"
+# end
+
+# # No need to modify this code! Use this to implement the fizzbuzz_printer method.
+# def fizzbuzz(num)
+#   if num % 3 == 0 && num % 5 == 0
+#     "FizzBuzz"
+#   elsif num % 3 == 0
+#     "Fizz"
+#   elsif num % 5 == 0
+#     "Buzz"
+#   else
+#     num
+#   end
+# end
+
+# def fizzbuzz_printer
+#   # your code here
+#   (1..100).each do |num|
+#     if num % 5 == 0 && num % 3 == 0
+#       puts "FizzBuzz"
+#     elsif num % 3 == 0
+#       puts "Fizz"
+#     elsif num % 5 == 0
+#       puts "Buzz"
+#     else
+#       puts "#{num}"
+#     end
+#   end
+# end
+
+# def reverse_string(str)
+#   # your code here
+#   s
+#   str.chars.reduce { |x, y| y + x }
+#   # i = str.length - 1
+#   # while i >= 0
+#   #   print char[i]
+#   #   i = i - 1
+#   # end
+# end
